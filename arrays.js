94f5002673b5ac2ec3f631524b3721c3fde64cb2
Saheel Wagh <saheelwagh@gmail.com>
2017-09-12 10:12:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,9 @@
 var chocolateBars =[ 'snickers', 'hundred grand', 'kitkat','skittles'];
 function destructivelyAddElementToBeginningOfArray(array,element){
-  
+  array.unshift(element);
+  return array;
+}
+function addElementToBeginningOfArray(array, element){
+  var newArray = [element, ...array];
+  return newArray;
 }
